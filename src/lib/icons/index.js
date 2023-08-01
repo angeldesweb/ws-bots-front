9ef0629cd9725c1dc032fae7b9ce0221b0327ae6
Proyecto-Simon-2 <alexparracol81@gmail.com>
2023-08-01 18:31:39 -0400

    session

--- src/lib/icons/index.js
+++ src/lib/icons/index.js
@@ -1,2 +1,15 @@
-export { default as Arrows } from './Arrows.svelte';
-export { default as Whatsapp } from './Whatsapp.svelte';
+export { default as Arrows } from './Icons-Seccion/Arrows.svelte';
+export { default as Whatsapp } from './Icons-Seccion/Whatsapp.svelte';
+export { default as Contactos } from './Icons-Seccion/Contactos.svelte';
+export { default as Respuestas } from './Icons-Seccion/Respuestas.svelte';
+export { default as Equipo } from './Icons-Seccion/Equipo.svelte';
+export { default as Chatbots} from './Icons-Seccion/Chatbots.svelte';
+export { default as Campaña } from './Icons-Seccion/Campaña.svelte';
+export { default as Informes } from './Icons-Seccion/Informes.svelte';
+export { default as Configuracion } from './Icons-Seccion/Configuracion.svelte';
+
+//ICONES DE MENU
+export { default as Sidebar } from './Icons-Menu/Sidebar.svelte';
+export { default as DarkOn } from './Icons-Menu/DarkOn.svelte';
+export { default as DarkOff } from './Icons-Menu/DarkOff.svelte';
+export { default as Lenguaje } from './Icons-Menu/Lenguaje.svelte';
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
-	import { Arrows, Whatsapp } from '$lib/icons';
+	import Whatsapp from '../../lib/icons/Icons-Seccion/Whatsapp.svelte'
+	import Arrows from '../../lib/icons/Icons-Seccion/Arrows.svelte';
+	import Contactos from '../../lib/icons/Icons-Seccion/Contactos.svelte';
+	import Campaa from '../../lib/icons/Icons-Seccion/Campaña.svelte';
+	import Chatbots from '../../lib/icons/Icons-Seccion/Chatbots.svelte';
+	import Equipo from '../../lib/icons/Icons-Seccion/Equipo.svelte';
+	import Respuestas from '../../lib/icons/Icons-Seccion/Respuestas.svelte';
+	import Informes from '../../lib/icons/Icons-Seccion/Informes.svelte';
+	import Configuracion from '../../lib/icons/Icons-Seccion/Configuracion.svelte';
+
 </script>
 
 <div class="drawer lg:drawer-open">
@@ -12,8 +21,18 @@
 		<label for="my-drawer-2" class="drawer-overlay" />
 		<!-- SIDEBAR -->
 		<ul class="menu p-4 w-80 h-full bg-base-200 shadow-xl">
+			<label class="menu-title"> PRINCIPAL </label>
 			<li><a href="/home"><Whatsapp /> Whatsapp</a></li>
 			<li><a href="/conexiones"><Arrows /> Conexiones</a></li>
+			<li><a href="/contactos"><Contactos /> Contactos</a></li>
+			<li><a href="/respuestas"><Respuestas /> Respuestas Rapidas</a></li>
+			<label class="menu-title"> ADMINISTRACIÓN </label>
+			<li><a href="/equipo"><Equipo /> Equipo</a></li>
+			<li><a href="/chatbots"><Chatbots /> Chatbots</a></li>
+			<li><a href="/campaña"><Campaa /> Campaña</a></li>
+			<li><a href="/informes"><Informes /> Informes</a></li>
+			<li><a href="/configuracion"><Configuracion /> Configuracion</a></li>
+
 		</ul>
 	</div>
 </div>
