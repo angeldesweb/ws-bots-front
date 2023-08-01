19869b2f568ccfc0553c11aaad016093c57cf2f2
Ángel Zamora <angeldesweb@gmail.com>
2023-08-01 19:14:14 -0400

    initial buttons config

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Whatsapp from '../../lib/icons/Icons-Seccion/Whatsapp.svelte'
+	import Whatsapp from '../../lib/icons/Icons-Seccion/Whatsapp.svelte';
 	import Arrows from '../../lib/icons/Icons-Seccion/Arrows.svelte';
 	import Contactos from '../../lib/icons/Icons-Seccion/Contactos.svelte';
 	import Campaa from '../../lib/icons/Icons-Seccion/Campaña.svelte';
@@ -8,7 +8,6 @@
 	import Respuestas from '../../lib/icons/Icons-Seccion/Respuestas.svelte';
 	import Informes from '../../lib/icons/Icons-Seccion/Informes.svelte';
 	import Configuracion from '../../lib/icons/Icons-Seccion/Configuracion.svelte';
-
 </script>
 
 <div class="drawer lg:drawer-open">
@@ -21,18 +20,17 @@
 		<label for="my-drawer-2" class="drawer-overlay" />
 		<!-- SIDEBAR -->
 		<ul class="menu p-4 w-80 h-full bg-base-200 shadow-xl">
-			<label class="menu-title"> PRINCIPAL </label>
+			<label for="" class="menu-title"> PRINCIPAL </label>
 			<li><a href="/home"><Whatsapp /> Whatsapp</a></li>
 			<li><a href="/conexiones"><Arrows /> Conexiones</a></li>
 			<li><a href="/contactos"><Contactos /> Contactos</a></li>
 			<li><a href="/respuestas"><Respuestas /> Respuestas Rapidas</a></li>
-			<label class="menu-title"> ADMINISTRACIÓN </label>
+			<label for="" class="menu-title"> ADMINISTRACIÓN </label>
 			<li><a href="/equipo"><Equipo /> Equipo</a></li>
 			<li><a href="/chatbots"><Chatbots /> Chatbots</a></li>
 			<li><a href="/campaña"><Campaa /> Campaña</a></li>
 			<li><a href="/informes"><Informes /> Informes</a></li>
 			<li><a href="/configuracion"><Configuracion /> Configuracion</a></li>
-
 		</ul>
 	</div>
 </div>
--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,18 +1,55 @@
 <script>
+	import { io } from 'socket.io-client';
+	import { onMount } from 'svelte';
+	import Qr from 'qrcode';
+	import { browser } from '$app/environment';
+
+	export let data;
+
+	let socket;
 	let canvas;
 	let hasCode = false;
+
+	$: browser &&
+		socket &&
+		socket.on('qr', ({ qr }) => {
+			hasCode = true;
+			Qr.toCanvas(canvas, qr);
+		});
+
+	$: browser &&
+		socket &&
+		socket.on('loading', ({ percent, message }) => {
+			console.log({ percent, message });
+		});
+
+	const init = () => {
+		socket = new io('http://127.0.0.1:3000', {
+			autoConnect: false,
+			auth: {
+				token: data.token
+			}
+		});
+
+		socket.connect();
+		socket.emit('start:client');
+	};
+
+	onMount(() => {
+		browser && init();
+	});
 </script>
 
 <svelte:head>
 	<title>Whatsapp bots</title>
 </svelte:head>
 
-<div>
-	<canvas class="hidden box rounded-box shadow-xl p-4 m-auto" bind:this={canvas} />
-</div>
 {#if !hasCode}
 	<span class="text-2xl">Generating Qr code</span>
 {/if}
+<div>
+	<canvas class="box {hasCode ? 'rounded-box shadow-xl p-4' : ''} m-auto" bind:this={canvas} />
+</div>
 
 <style>
 	.box {
