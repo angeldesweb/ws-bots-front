7d1304c4444a31b881d7bebafba5f09d3fa6a95f
Ángel Zamora <angeldesweb@gmail.com>
2023-08-02 21:49:37 -0400

    whatsapp basic implementations

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,6 +1,13 @@
 export const handle = async ({ event, resolve }) => {
 	const token = event.cookies.get('token') || null;
-	//console.log(token);
+
+	if (token && event.url.pathname === '/') {
+		return new Response('Redirect', { status: 301, headers: { Location: '/home' } });
+	}
+
+	if (!token && event.url.pathname !== '/') {
+		return new Response('Redirect', { status: 301, headers: { Location: '/' } });
+	}
 
 	return resolve(event);
 };
